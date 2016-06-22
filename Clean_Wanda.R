5b54ab98659f15f564d99e3327f32cd9b856c903
Ho Fai Wong <hofai.wong85@gmail.com>
2016-06-22 17:56:50 -0400

    Updated CleanWanda to have shuffle vs original

--- Clean_Wanda.R
+++ Clean_Wanda.R
@@ -18,11 +18,12 @@ KDD.test <-read.csv("./data/KDDTest+.csv", header = FALSE,
                     stringsAsFactors = FALSE)
 colnames(KDD.test) <- column.names #Rename columns
 
+#Combine train+test, shuffle and split
 KDD.shuffle = rbind(KDD.train, KDD.test)
 set.seed(0)
 shuffle.train = sample(1:nrow(KDD.shuffle), nrow(KDD.train))
-new.KDD.train = KDD.shuffle[shuffle.train, ]
-new.KDD.test = KDD.shuffle[-shuffle.train, ]
+new.KDD.train.shuffle = KDD.shuffle[shuffle.train, ]
+new.KDD.test.shuffle = KDD.shuffle[-shuffle.train, ]
 
 #Function to prep, munge and dummify train and test data
 prep = function(df) {
@@ -41,48 +42,64 @@ prep = function(df) {
   return(new.df)
 }
 
-#Run function on train
-new.KDD.train = prep(new.KDD.train) #84 dummy features, new dim: 125973 123
+#Run function on train, test, train.shuffle, test.shuffle
+new.KDD.train = prep(KDD.train) #84 dummy features, new dim: 125973 123
 mean(new.KDD.train$outcome.response==1) #46.5% malicious connections
-
-#Run function on test
-new.KDD.test = prep(new.KDD.test) #77 dummy features, new dim: 22543 116
+new.KDD.test = prep(KDD.test) #77 dummy features, new dim: 22543 116
 mean(new.KDD.test$outcome.response==1) #56.9% malicious connections
 
+new.KDD.train.shuffle = prep(new.KDD.train.shuffle) #84 dummy features, new dim: 125973 123
+mean(new.KDD.train.shuffle$outcome.response==1) #0.4809205
+new.KDD.test.shuffle = prep(new.KDD.test.shuffle) #80 dummy; new dim 22543, 119
+mean(new.KDD.test.shuffle$outcome.response==1) #0.4826332
+
 #Comparing columns in test and train
-a = sapply(colnames(new.KDD.test), function(i) ifelse(i %in% colnames(new.KDD.train), TRUE, FALSE))
-which(a==FALSE)
+# a = sapply(colnames(new.KDD.test), function(i) ifelse(i %in% colnames(new.KDD.train), TRUE, FALSE))
+# which(a==FALSE)
 b = sapply(colnames(new.KDD.train), function(i) ifelse(i %in% colnames(new.KDD.test), TRUE, FALSE))
 which(b==FALSE)
 
 #Remove columns with only one value: num_outbound_cmds in train and test
-for (i in 1:dim(new.KDD.test)[2]) {
-  if (length(unique(new.KDD.test[,i])) < 2) {
-    cat(names(new.KDD.test)[i],'\n')
-  }
-}
+# check_constant = function(df) {
+#   for (i in 1:dim(df)[2]) {
+#     if (length(unique(df[,i])) < 2) {
+#       cat(names(df)[i],'\n')
+#     }
+#   }
+# }
+# check_constant(new.KDD.train)
+# check_constant(new.KDD.test)
+# check_constant(new.KDD.test.shuffle)
+# check_constant(new.KDD.test.shuffle)
 new.KDD.train = subset(new.KDD.train, select=-c(num_outbound_cmds))
 new.KDD.test = subset(new.KDD.test, select=-c(num_outbound_cmds))
+new.KDD.train.shuffle = subset(new.KDD.train.shuffle, select=-c(num_outbound_cmds))
+new.KDD.test.shuffle = subset(new.KDD.test.shuffle, select=-c(num_outbound_cmds))
 
-#Group missing levels in train and add grouped column in test
-new.KDD.train$grp.lvl = 0
-for (i in names(b[b==FALSE])) {
-  for (j in 1:nrow(new.KDD.train)){
-    if (new.KDD.train[j,i]==1) {
-      new.KDD.train$grp.lvl[j]=1  
-    }
-  }
-}
-new.KDD.train = subset(new.KDD.train, select=-which(b==FALSE))
-new.KDD.test$grp.lvl = 0
-
-# #Add missing levels in test, set to 0, and reorder to match train
+# #Group missing levels in train and add grouped column in test
+# new.KDD.train$grp.lvl = 0
 # for (i in names(b[b==FALSE])) {
-#   new.KDD.test[,i] = 0
+#   for (j in 1:nrow(new.KDD.train)){
+#     if (new.KDD.train[j,i]==1) {
+#       new.KDD.train$grp.lvl[j]=1  
+#     }
+#   }
 # }
-# new.KDD.test = new.KDD.test[colnames(new.KDD.train)]
+# new.KDD.train = subset(new.KDD.train, select=-which(b==FALSE))
+# new.KDD.test$grp.lvl = 0
+
+#Add missing levels in test, set to 0, and reorder to match train
+for (i in names(b[b==FALSE])) {
+  new.KDD.test[,i] = 0
+  new.KDD.test.shuffle[,i] = 0
+}
+new.KDD.test = new.KDD.test[colnames(new.KDD.train)]
+new.KDD.test.shuffle = new.KDD.test.shuffle[colnames(new.KDD.train)]
+
 
 #Check if columns match between train and test
 names(new.KDD.test)==names(new.KDD.train)
+names(new.KDD.test.shuffle)==names(new.KDD.train.shuffle)
 
-rm(a, b, i)
+
+rm(b, i)
--- HF/LogisticRegression/glmnet.R
+++ HF/LogisticRegression/glmnet.R
@@ -7,7 +7,6 @@ library(glmnet)
 library(car)
 library(MASS)
 
-
 ###################
 #### Functions ####
 ###################
@@ -31,9 +30,17 @@ accuracy = function(target, pred) {
   return(contingency)
 }
 
+
+
 #########################
 #### Scale and split ####
 #########################
+
+#Define standardization function for train
+Standard_self<-function(data) {
+  data=(data-mean(data))/sd(data)
+  return (data)
+}
 
 #Finding binary columns to exclude from scaling
 bin.col = data.frame(matrix(ncol = 4, nrow = dim(new.KDD.train)[2]-1))
@@ -44,20 +51,25 @@ for (i in 1:(dim(new.KDD.train)[2]-1)) {
   bin.col[i,3]=max(new.KDD.train[,i])
   bin.col[i,4]=min(new.KDD.train[,i])
 }
-bin.col[bin.col$unique==2,]
+#bin.col[bin.col$unique==2,]
 bin.col = bin.col[bin.col$unique==2,1]
 
 #Scaling non-binary columns in train
-new.KDD.train.scaled = sapply(new.KDD.train[,-c(bin.col,116)], Standard_self)
-new.KDD.train.scaled = cbind(new.KDD.train[,c(bin.col,116)], new.KDD.train.scaled)
+new.KDD.train.scaled = sapply(new.KDD.train[,-c(bin.col,122)], Standard_self)
+new.KDD.train.scaled = cbind(new.KDD.train[,c(bin.col,122)], new.KDD.train.scaled)
 new.KDD.train.scaled = new.KDD.train.scaled[,colnames(new.KDD.train)]
 
 #Scaling non-binary columns in test
-ave = sapply(new.KDD.train[,-c(bin.col,116)], mean)
-sd = sapply(new.KDD.train[,-c(bin.col,116)], sd)
-new.KDD.test.scaled = sapply(new.KDD.test[,-c(bin.col,116)], function(x) Standard(x,ave,sd))
-new.KDD.test.scaled = cbind(new.KDD.test[,c(bin.col,116)], new.KDD.test.scaled)
+ave = sapply(new.KDD.train[,-c(bin.col,122)], mean)
+sd = sapply(new.KDD.train[,-c(bin.col,122)], sd)
+new.KDD.test.scaled = sapply(new.KDD.test[,-c(bin.col,122)], function(x) Standard(x,ave,sd))
+new.KDD.test.scaled = cbind(new.KDD.test[,c(bin.col,122)], new.KDD.test.scaled)
 new.KDD.test.scaled = new.KDD.test.scaled[,colnames(new.KDD.test)]
+
+
+#####################
+#### Logit Lasso ####
+#####################
 
 #Creating the data matrices for the glmnet() function.
 x.train = model.matrix(outcome.response ~ ., new.KDD.train.scaled)[, -1]
@@ -67,8 +79,7 @@ y.test = new.KDD.test.scaled$outcome.response
 
 #Creating training and test sets
 set.seed(0)
-train = sample(1:nrow(x), 7*nrow(x)/10)
-
+train = sample(1:nrow(x.train), 7*nrow(x.train)/10)
 
 #Fitting the logistic regression on a grid of lambda. Alpha = 1 for lasso penalty
 grid = 10^seq(0, -5, length = 200)
@@ -86,17 +97,17 @@ grid = 10^seq(0, -5, length = 200)
 set.seed(0)
 logit.cv = cv.glmnet(x.train[train, ], y.train[train], 
                      alpha = 1,            #Lasso penalty
-                     nfolds = 3,           #k-fold CV
+                     nfolds = 10,          #k-fold CV
                      type.measure='class', #Misclassification measure
                      family="binomial",    #Logistic regression
                      lambda = grid)
-# plot(logit.cv, main = "Logistic Regression with Lasso penalty\n")
+plot(logit.cv, main = "Logistic Regression with Lasso penalty\n")
 
 
 #Best lambda
-logit.cv$lambda.min #1.122668e-05 with k=5; 1.189534e-05 with k=10
-log(logit.cv$lambda.min) #-11.39722 with k=5; -11.33936 with k=10
-lambda = exp(-3) #lamdba.min still keeps ~110 features. Need to balance complexity and accuracy
+logit.cv$lambda.min #1.122668e-05 with k=5; 1.189534e-05 with k=10; 9.011018e-05 with k=10 and shuffled data
+log(logit.cv$lambda.min) #-11.39722 with k=5; -11.33936 with k=10; -9.314477 with k=10 and shuffled data
+lambda = exp(-4) #lamdba.min still keeps ~100 features. Need to balance complexity and accuracy
 
 
 #Checking accuracy of model - train data, test subset
@@ -104,37 +115,39 @@ logit.test.class = predict(logit.cv,
                            s = lambda, 
                            type = 'class',
                            newx = x.train[-train, ])
-accuracy(y.train[-train], logit.test.class) #94.5% accuracy with lambda=exp(-3); 97.6% with lambda.min
+accuracy(y.train[-train], logit.test.class) 
+#94.5% accuracy with lambda=exp(-3); 97.6% with lambda.min
 #lambda=exp(-3): 94.5% (seed=0), k=5
 #lambda.min: 97.6% (seed=0), k=5
 #lambda=exp(-3): 0.9451471 (seed=100), k=5
 #lambda=exp(-3): 0.9445914 (seed=30), k=5
 #lambda=exp(-3): 0.9447767 (seed=0), k=10
-#Shuffled: 0.9172047
+#Shuffled, exp(-4), k=10: 0.9402784 
 
 #Checking accuracy of model - test data
 logit.test.class.final = predict(logit.cv, 
                                  s = lambda, 
                                  type = 'class',
                                  newx = x.test)
-accuracy(y.test, logit.test.class.final) #69.8% accuracy with lambda=exp(-3)
+accuracy(y.test, logit.test.class.final) 
+#69.8% accuracy with lambda=exp(-3)
 #lambda=exp(-3): 69.8% (seed=0), k=5
 #lambda.min: 
 #lambda=exp(-3): 0.6972896% (seed=100), k=5
 #lambda=exp(-3): 0.6970235 (seed=30), k=5
 #lambda=exp(-3): 0.6978219 (seed=0), k=10
-#Shuffled: 0.8559198
+#Shuffled, exp(-4), k=10: 0.868651 
 
 #Coefficients: 
 logit.coef = predict(logit.cv, 
                      s = lambda, 
                      type = 'coefficients')
-sum(logit.coef!=0)-1 #Keep 110 features with lambda.min, 12 with lambda=exp(-3)
+sum(logit.coef!=0)-1 #Keep ~110 features with lambda.min, 23 with lambda=exp(-4)
 logit.coef
 logit.nonzero = predict(logit.cv, 
                         s = lambda, 
                         type = 'nonzero')
-colnames(x)[logit.nonzero[,1]]
+colnames(x.train)[logit.nonzero[,1]]
 
 ##########################
 #### Accuracy Summary ####
